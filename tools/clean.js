ff2cd800b38a25baab053a205240ad9159f14ad8
the-j0k3r <31389848+the-j0k3r@users.noreply.github.com>
2020-03-10 10:25:06 +0000

    add perfectionist/clean

---
+++ tools/clean.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+"use strict";
+
+const fs = require("fs").promises;
+const path = require("path");
+const pkg = require("../package.json");
+
+const fileName = path.join(__dirname, "..", pkg.main);
+
+function cleanup(css) {
+  return css
+    // Perfectionist adds comments to the end of the previous line...
+    // }/* comment */ => }\n\n  /* comment */
+    .replace(/}\/\*(([\s\S])+?)\*\/\s*/g, "}\n\n  /*$1*/\n  ")
+    .replace(/,\s\/\*/g, ",\n  /*")
+    // Remove leading whitespace from @-moz-document entries
+    .replace(/\n\s{15}/g, "\n")
+}
+
+async function postPerfectionist() {
+  const css = await fs.readFile(fileName, "utf8");
+  await fs.writeFile(fileName, cleanup(css));
+  console.info("\u001B[32m%s\u001B[0m", `${pkg.title} usercss cleanup completed`);
+}
+
+postPerfectionist();
